793893866b4aa597203698754a3c6d4ff050ec2c
YA323065 <yadab.nepal@wipro.com>
2019-07-05 17:30:01 +0530

    materialize

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="#" className="brand-logo">Emaily</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><a href="/auth/google">Login with Google</a></li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Header;
--- services/passport.js
+++ services/passport.js
@@ -22,35 +22,30 @@ passport.use(
         clientID:keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({googleId: profile.id})
-            .then((existingUser) => {
-                if (existingUser) {
-                    done(null, existingUser);
-                } else {
-                    new User({googleId: profile.id })
-                        .save()
-                        .then(user => done(null, user));
-                }
-            });
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({googleId: profile.id});
+        if (existingUser) {
+            return done(null, existingUser);
+        }
+        const user = await new User({googleId: profile.id }).save();
+        done(null, user);
     })
 );
 
 
-passport.use(new LinkedInStrategy({
-    clientID: keys.linkedinClientID,
-    clientSecret: keys.linkedinClientSecret,
-    callbackURL: "http://localhost:5000/auth/linkedin/callback/",
-    scope: ['r_emailaddress', 'r_liteprofile'],
-}, function(accessToken, refreshToken, profile, done) {
-    User.findOne({googleId: profile.id})
-        .then((existingUser) => {
-            if (existingUser) {
-                done(null, existingUser);
-            } else {
-                new User({googleId: profile.id })
-                    .save()
-                    .then(user => done(null, user));
-            }
-        });
+passport.use(
+    new LinkedInStrategy({
+        clientID: keys.linkedinClientID,
+        clientSecret: keys.linkedinClientSecret,
+        callbackURL: "http://localhost:5000/auth/linkedin/callback/",
+        scope: ['r_emailaddress', 'r_liteprofile'],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({googleId: profile.id});
+        if (existingUser) {
+            return done(null, existingUser);
+        }
+        const user = await new User({googleId: profile.id }).save();
+        done(null, user);
 }));
