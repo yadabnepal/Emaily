9872b3272315afb28008a51651ca65d7bd5a58c9
YA323065 <yadab.nepal@wipro.com>
2019-08-06 09:02:50 +0530

    final

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,9 +10,18 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
-      res.send("Thanks for voting");
-  });
+    app.get("/api/surveys/:surveyId/:choice", (req, res) => {
+        res.send("Thanks for voting");
+    });
+
+    app.get('/api/surveys', requireLogin, async (req, res)=> {
+        const surveys = await Survey.find({
+            _user: req.user.id
+        })
+            .select({recipients: false});
+
+        res.send(surveys);
+    });
 
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
@@ -43,7 +52,7 @@ module.exports = app => {
 
     app.post('/api/surveys/webhooks', (req, res) => {
        const p = new Path('/api/surveys/:surveyId/:choice');
-       const events = _.chain(req.body)
+       _.chain(req.body)
            .map(({email, url})=> {
           const match = p.test(new URL(url).pathname);
           if (match) {
@@ -51,8 +60,20 @@ module.exports = app => {
           }
        })
        .compact()
-       .uniqBy('email', 'surveyId');
-       console.log(events);
+       .uniqBy('email', 'surveyId')
+       .each(({ surveyId, email, choice }) => {
+           Survey.updateOne({
+               _id: surveyId,
+               recipients: {
+                   $elementMatch: {email, responded: false}
+               }
+           }, {
+               $inc: {[choice]: 1},
+               $set: {'recipients.$.responded': true},
+               lastResponded: new Date()
+           }).exec();
+       })
+       .value();
 
        res.send({});
     });
